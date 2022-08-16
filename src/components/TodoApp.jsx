6acad88b98ed05a756856cd609532674e9304e1c
Dario <52776239+iodario@users.noreply.github.com>
2022-08-16 04:17:07 -0300

    from codesandbox

--- src/components/TodoApp.jsx
+++ src/components/TodoApp.jsx
@@ -1,13 +1,11 @@
-import '../styles/TodoApp.css'
+import "../styles/TodoApp.css";
 
 function TodoApp() {
-  
-
   return (
     <div className="TodoContainer">
-    <h1>Hola</h1>
+      <h1>Hola que onda todo bien, todo piola ran</h1>
     </div>
-  )
+  );
 }
 
 export default TodoApp;
